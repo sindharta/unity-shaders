b7db87d433b1b42bef879fa67aac8d766ae268fe
sin <sindharta.tanuwijaya@unity3d.com>
2024-09-25 18:08:16 +0900

    add blitters

---
+++ Packages/ShinCore/Runtime/Scripts/TextureBlitters/LegacyTextureBlitter.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Unity.StreamingImageSequence {
+
+[ExecuteAlways]
+[RequireComponent(typeof(Camera))]
+internal class LegacyTextureBlitter : BaseTextureBlitter {
+
+    protected override void AwakeInternalV() { }
+
+//----------------------------------------------------------------------------------------------------------------------    
+    
+    void OnRenderImage(RenderTexture source, RenderTexture destination) {
+        BlitToDest(destination);
+    }    
+    
+}
+
+} //end namespace
